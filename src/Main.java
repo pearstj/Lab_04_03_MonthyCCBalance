2ef20989be6b9da7299ad9af26213d5666ebfeb4
pearstj <pearstj@mail.uc.edu>
2025-09-13 22:42:33 -0400

    tested

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+class CreditCardInterest {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Initial balance
+        double balance = 5000.00;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Annual interest rate
+        final double ANNUAL_RATE = 0.17;
+
+        // Monthly interest rate
+        double monthlyRate = ANNUAL_RATE / 12;
+
+        // Interest after 1 month
+        double month1Balance = balance * (1 + monthlyRate);
+        double interestMonth1 = month1Balance - balance;
+
+        // Interest after 2 months
+        double month2Balance = month1Balance * (1 + monthlyRate);
+        double interestMonth2 = month2Balance - balance;
+
+        // Output the results
+        System.out.println("Initial Credit Card Balance: $" + String.format("%.2f", balance));
+        System.out.println("Annual Interest Rate: 17%");
+
+        System.out.println("\nAfter 1 Month:");
+        System.out.printf("  Interest Due: $%.2f%n", interestMonth1);
+        System.out.printf("  New Balance:  $%.2f%n", month1Balance);
+
+        System.out.println("\nAfter 2 Months:");
+        System.out.printf("  Total Interest Due: $%.2f%n", interestMonth2);
+        System.out.printf("  New Balance:        $%.2f%n", month2Balance);
     }
 }
